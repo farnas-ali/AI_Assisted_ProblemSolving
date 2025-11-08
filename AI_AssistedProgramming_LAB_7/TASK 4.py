2c079d473a56e4e4e010f040ad4725e3d091ccb7
farnas-ali <mohdfarnasali@gmail.com>
2025-11-08 23:31:04 +0530

    Add files via upload

---
+++ AI_AssistedProgramming_LAB_7/TASK 4.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+# Taking user input
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+r = Rectangle(length, width)
+print("Length:", r.length)
+print("Width:", r.width)
